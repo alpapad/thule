70e5c4905de0b0aab20de5ed763f12e42f917abc
wayne <wayne@serin-consultancy.co.uk>
2016-07-17 15:55:18 +0100

    Upgraded gosu to 1.9

--- thule-config-service/src/main/docker/docker-entrypoint.sh
+++ thule-config-service/src/main/docker/docker-entrypoint.sh
@@ -10,7 +10,7 @@ if [ "$1" = "app" ]; then
     # Use a non-blocking entropy source (i.e. /dev/urandom) instead of the default blocking entropy source (i.e. /dev/random)
     # to reduce the startup time of Tomcat which is used by Spring boot as the default web container
     # See http://wiki.apache.org/tomcat/HowTo/FasterStartUp#Entropy_Source
-    exec gosu $USER java -Djava.security.egd=file:/dev/./urandom -jar $jarFileName
+    exec su-exec $USER java -Djava.security.egd=file:/dev/./urandom -jar $jarFileName
 fi
 
 exec "$@"
