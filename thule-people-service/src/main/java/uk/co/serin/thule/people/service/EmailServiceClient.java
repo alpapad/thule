5f0a7ae90f71f3038183dc5cd9e587f921cb251e
wayne <wayne@serin-consultancy.co.uk>
2017-06-24 20:25:33 +0100

    Fixed SonarQube issue

--- thule-people-service/src/main/java/uk/co/serin/thule/people/service/EmailServiceClient.java
+++ thule-people-service/src/main/java/uk/co/serin/thule/people/service/EmailServiceClient.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import uk.co.serin.thule.people.domain.email.Email;
 
 @FeignClient("thule-email-service")
+@FunctionalInterface
 public interface EmailServiceClient {
     @RequestMapping(path = "/emails", headers = "Content-Type=application/json", method = RequestMethod.POST)
     Email create(Email email);
