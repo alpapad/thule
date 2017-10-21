a48e5f99b917e4d336c270410131c18dde180f06
wayne <wayne@serin-consultancy.co.uk>
2017-10-21 10:56:24 +0100

    Removed spurious debug statements

--- thule-config-service/src/main/docker/docker-entrypoint.sh
+++ thule-config-service/src/main/docker/docker-entrypoint.sh
@@ -10,8 +10,6 @@ if [ "$1" = "app" ]; then
     # Use a non-blocking entropy source (i.e. /dev/urandom) instead of the default blocking entropy source (i.e. /dev/random)
     # to reduce the startup time of Tomcat which is used by Spring boot as the default web container
     # See http://wiki.apache.org/tomcat/HowTo/FasterStartUp#Entropy_Source
-    ls -al /
-    ls -al /dev/./urandom
     exec chroot --skip-chdir --userspec $USER:$USER / java -Djava.security.egd=file:/dev/./urandom -jar $jarFileName
 fi
 
