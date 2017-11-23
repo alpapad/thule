569bea661317edc5566a02a08eb4ab7fc39116b9
wayne <wayne@serin-consultancy.co.uk>
2017-11-23 07:44:30 +0000

    Added explicit @Async annotation required when migrating from spring boot 1.5.x to 2.0.x

--- thule-email-service/src/main/java/uk/co/serin/thule/email/Application.java
+++ thule-email-service/src/main/java/uk/co/serin/thule/email/Application.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @ComponentScan
+@EnableAsync
 @EnableDiscoveryClient
 public class Application {
     private static SpringApplication springApplication = new SpringApplication(Application.class);
