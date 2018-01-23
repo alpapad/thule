dc05baf95bc7be9df1775429d2284728b87ccf71
wayne <wayne@serin-consultancy.co.uk>
2018-01-23 15:29:23 +0000

    Removed redundant spring boot annotations

--- thule-email-service/src/main/java/uk/co/serin/thule/email/Application.java
+++ thule-email-service/src/main/java/uk/co/serin/thule/email/Application.java
@@ -2,11 +2,9 @@ package uk.co.serin.thule.email;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
-@ComponentScan
 @EnableAsync
 public class Application {
     private static SpringApplication springApplication = new SpringApplication(Application.class);
