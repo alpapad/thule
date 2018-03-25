52c727d45234ce6b777265b6820927615a363fe9
wayne <wayne@serin-consultancy.co.uk>
2018-03-25 17:58:06 +0000

    Moved spring configuration annotations to a new ApplicationConfigurer class

--- thule-repository-mongodb/src/test/java/uk/co/serin/thule/repository/mongodb/ApplicationConfigurerTest.java
+++ thule-repository-mongodb/src/test/java/uk/co/serin/thule/repository/mongodb/ApplicationConfigurerTest.java
@@ -4,6 +4,8 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.data.mongodb.core.mapping.event.ValidatingMongoEventListener;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -21,5 +23,27 @@ public class ApplicationConfigurerTest {
 
         // Then
         assertThat(applicationConfigurer).isNotNull();
+    }
+
+    @Test
+    public void localValidatorFactoryBean_is_configured() {
+        // Given
+
+        // When
+        LocalValidatorFactoryBean localValidatorFactoryBean = applicationConfigurer.localValidatorFactoryBean();
+
+        // Then
+        assertThat(localValidatorFactoryBean).isNotNull();
+    }
+
+    @Test
+    public void validatingMongoEventListener_is_configured() {
+        // Given
+
+        // When
+        ValidatingMongoEventListener validatingMongoEventListener = applicationConfigurer.validatingMongoEventListener();
+
+        // Then
+        assertThat(validatingMongoEventListener).isNotNull();
     }
 }
--- thule-repository-mongodb/src/test/java/uk/co/serin/thule/repository/mongodb/ApplicationTest.java
+++ thule-repository-mongodb/src/test/java/uk/co/serin/thule/repository/mongodb/ApplicationTest.java
@@ -1,8 +1,6 @@
 package uk.co.serin.thule.repository.mongodb;
 
 import org.junit.Test;
-import org.springframework.data.mongodb.core.mapping.event.ValidatingMongoEventListener;
-import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -10,29 +8,5 @@ public class ApplicationTest {
     @Test
     public void default_constructor_creates_instance_successfully() {
         assertThat(new Application()).isNotNull();
-    }
-
-    @Test
-    public void localValidatorFactoryBean_is_configured() {
-        // Given
-        Application mongodbConfiguration = new Application();
-
-        // When
-        LocalValidatorFactoryBean localValidatorFactoryBean = mongodbConfiguration.localValidatorFactoryBean();
-
-        // Then
-        assertThat(localValidatorFactoryBean).isNotNull();
-    }
-
-    @Test
-    public void validatingMongoEventListener_is_configured() {
-        // Given
-        Application mongodbConfiguration = new Application();
-
-        // When
-        ValidatingMongoEventListener validatingMongoEventListener = mongodbConfiguration.validatingMongoEventListener();
-
-        // Then
-        assertThat(validatingMongoEventListener).isNotNull();
     }
 }
