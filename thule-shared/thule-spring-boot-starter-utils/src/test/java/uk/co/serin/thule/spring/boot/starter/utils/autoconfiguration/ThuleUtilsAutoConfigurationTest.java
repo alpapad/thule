b01af85b85beb35dc7cb5c1c9445eb78b0f33514
wayne <wayne@serin-consultancy.co.uk>
2018-04-06 13:24:34 +0100

    Added unit test

---
+++ thule-shared/thule-spring-boot-starter-utils/src/test/java/uk/co/serin/thule/spring/boot/starter/utils/autoconfiguration/ThuleUtilsAutoConfigurationTest.java
@@ -0,0 +1,16 @@
+package uk.co.serin.thule.spring.boot.starter.utils.autoconfiguration;
+
+import org.junit.Test;
+
+public class ThuleUtilsAutoConfigurationTest {
+
+    @Test
+    public void class_under_test_instantiates_successfully() {
+        // Given
+
+        // When
+        new ThuleUtilsAutoConfiguration();
+
+        // Then
+    }
+}
