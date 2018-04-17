f2ead3370024a9781f0dd540d8066e6619997693
wayne <wayne@serin-consultancy.co.uk>
2018-04-17 11:31:01 +0100

    Added NewSpan annotation for repositories

--- thule-people-service/src/main/java/uk/co/serin/thule/people/repository/repositories/PersonRepository.java
+++ thule-people-service/src/main/java/uk/co/serin/thule/people/repository/repositories/PersonRepository.java
@@ -1,5 +1,6 @@
 package uk.co.serin.thule.people.repository.repositories;
 
+import org.springframework.cloud.sleuth.annotation.NewSpan;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
@@ -12,11 +13,14 @@ import java.util.Set;
 @RepositoryRestResource(collectionResourceRel = "people", path = "people")
 public interface PersonRepository extends ThuleRepository<Person, Long>, PersonRepositoryCustom {
     @Query("SELECT person FROM Person person LEFT JOIN FETCH person.roles roles LEFT JOIN FETCH person.photographs photographs LEFT JOIN FETCH person.state state LEFT JOIN FETCH state.actions actions  WHERE person.id = :id")
+    @NewSpan
     Person findByIdAndFetchAllAssociations(@Param("id") Long id);
 
     @Query("SELECT person FROM Person person LEFT JOIN FETCH person.photographs photographs WHERE person.updatedBy = :updatedBy")
+    @NewSpan
     Set<Person> findByUpdatedBy(@Param("updatedBy") String updatedBy);
 
     @Query("SELECT person FROM Person person LEFT JOIN FETCH person.roles roles LEFT JOIN FETCH person.photographs photographs LEFT JOIN FETCH person.state state LEFT JOIN FETCH state.actions actions  WHERE person.userId = :userId")
+    @NewSpan
     Person findByUserIdAndFetchAllAssociations(@Param("userId") String userId);
 }
