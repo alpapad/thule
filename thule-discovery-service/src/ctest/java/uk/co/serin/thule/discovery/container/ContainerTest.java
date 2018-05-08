9c8fb04b8092f6ba26430114a0a8da704d6fe71a
wayne <wayne@serin-consultancy.co.uk>
2018-05-08 12:41:18 +0100

    Added comments for integration and container tests

--- thule-discovery-service/src/ctest/java/uk/co/serin/thule/discovery/container/ContainerTest.java
+++ thule-discovery-service/src/ctest/java/uk/co/serin/thule/discovery/container/ContainerTest.java
@@ -1,5 +1,6 @@
 package uk.co.serin.thule.discovery.container;
 
+import org.awaitility.Duration;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -8,8 +9,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.actuate.health.Status;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.core.env.Environment;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -22,6 +23,22 @@ import static org.awaitility.Awaitility.given;
 import static org.awaitility.pollinterval.FixedPollInterval.fixed;
 import static uk.co.serin.thule.test.assertj.ThuleAssertions.assertThat;
 
+/**
+ * Tests for the Discovery service.
+ *
+ * The objective of these tests is to prove that a service registered within the Discovery Service can be retrieved and executed successfully:
+ *
+ * <ul>
+ *      <li>Start the discovery service</li>
+ *      <li>Register a target service in the Discovery Service</li>
+ *      Rather than create another service to act as the target to test the Discovery Service, we will use the Discovery Service itself as the target to test the Discovery Service.
+ *      To that end the Discovery Service has been configured for these tests to register itself at startup.
+ *      <li>Retrieve a target service from the Discovery Service</li>
+ *      In other words, retrieve the Discovery Service to act as the target from the Discovery Service
+ *      <li>Invoke an endpoint on the target service</li>
+ *      Use the actuator on the target Discovery Service
+ * </ul>
+ */
 @ActiveProfiles({"ctest", "${spring.profiles.include:default}"})
 @SpringBootTest
 @RunWith(SpringRunner.class)
@@ -30,7 +47,7 @@ public class ContainerTest {
     @Autowired
     private ActuatorClient actuatorClient;
     @Autowired
-    private Environment env;
+    private DiscoveryClient discoveryClient;
 
     @BeforeClass
     public static void setUpClass() throws IOException {
@@ -45,10 +62,10 @@ public class ContainerTest {
     @Test
     public void can_invoke_a_service_via_discovery() {
         // Given
-        given().ignoreExceptions().pollInterval(fixed(org.awaitility.Duration.FIVE_SECONDS)).
-                await().timeout(org.awaitility.Duration.FIVE_MINUTES).
+        given().ignoreExceptions().pollInterval(fixed(Duration.FIVE_SECONDS)).
+                await().timeout(Duration.FIVE_MINUTES).
                 untilAsserted(() -> {
-                    actuatorClient.health();
+                    assertThat(discoveryClient.getServices()).contains("gohenry-discovery-service");
                 });
 
         // When
