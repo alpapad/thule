714f909c3e2e5368999f6ad913fb3ec4bac4a8fb
wayne <wayne@serin-consultancy.co.uk>
2018-05-17 15:18:15 +0100

    Refactoring of spring config

--- thule-admin-server/src/ctest/java/uk/co/serin/thule/admin/container/ContainerTest.java
+++ thule-admin-server/src/ctest/java/uk/co/serin/thule/admin/container/ContainerTest.java
@@ -15,7 +15,7 @@ import java.time.Duration;
 import static uk.co.serin.thule.test.assertj.ThuleAssertions.assertThat;
 
 public class ContainerTest {
-    private static final ActuatorUri ADMIN_SERVER_STATUS_URI = new ActuatorUri(URI.create("http://172.17.0.1:8092/health"));
+    private static final ActuatorUri ADMIN_SERVER_STATUS_URI = new ActuatorUri(URI.create("http://172.17.0.1:8093/health"));
     private static DockerCompose dockerCompose = new DockerCompose("src/ctest/docker/thule-admin-server-container-tests/docker-compose.yml");
 
     @BeforeClass
