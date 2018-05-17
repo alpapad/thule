2af0fa07f5b810f909bfcbd004929b3fa2c9efbb
wayne <wayne@serin-consultancy.co.uk>
2018-05-17 15:13:49 +0100

    Refactoring of spring config

--- thule-admin-server/src/itest/java/uk/co/serin/thule/admin/e2e/AdminIntTest.java
+++ thule-admin-server/src/itest/java/uk/co/serin/thule/admin/e2e/AdminIntTest.java
@@ -11,14 +11,16 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles({"itest", "${spring.profiles.include:default}"})
 @RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class AdminIntTest {
     @Autowired
     private TestRestTemplate restTemplate;
