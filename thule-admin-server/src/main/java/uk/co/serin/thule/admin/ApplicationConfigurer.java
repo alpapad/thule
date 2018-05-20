6129994d2ee6178dc20701b413ad975e5eac81fe
wayne <wayne@serin-consultancy.co.uk>
2018-05-20 08:12:08 +0100

    Upgraded spring bot admin [1.5.7 -> 2.0.0]

--- thule-admin-server/src/main/java/uk/co/serin/thule/admin/ApplicationConfigurer.java
+++ thule-admin-server/src/main/java/uk/co/serin/thule/admin/ApplicationConfigurer.java
@@ -2,12 +2,10 @@ package uk.co.serin.thule.admin;
 
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @EnableAdminServer
-@EnableDiscoveryClient
 public class ApplicationConfigurer {
 }
 
