dd02ef03f86b2f15a14566b195a9cad990886dbe
wayne <wayne@serin-consultancy.co.uk>
2018-05-21 22:03:16 +0100

    Refactoring of spring config

---
+++ thule-configuration-service/src/itest/java/uk/co/serin/thule/configuration/e2e/ConfigIntTest.java
@@ -0,0 +1,33 @@
+package uk.co.serin.thule.configuration.e2e;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.actuate.health.Status;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import uk.co.serin.thule.test.assertj.ActuatorUri;
+
+import java.net.URI;
+
+import static uk.co.serin.thule.test.assertj.ThuleAssertions.assertThat;
+
+@ActiveProfiles({"itest", "${spring.profiles.include:default}"})
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class ConfigIntTest {
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void is_status_up() {
+        // Given
+        ActuatorUri actuatorUri = new ActuatorUri(URI.create(restTemplate.getRootUri() + "/actuator/health"));
+
+        // When/Then
+        assertThat(actuatorUri).waitingForMaximum(java.time.Duration.ofMinutes(5)).hasStatus(Status.UP);
+    }
+}
