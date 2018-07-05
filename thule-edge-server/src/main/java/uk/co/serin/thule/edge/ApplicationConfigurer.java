8c75e4059348ce50b42ca79e2efd216dd121cbd6
wayne <wayne@serin-consultancy.co.uk>
2018-07-05 08:57:52 +0100

    Refactored thule-edge-server to thule-gateway

--- thule-edge-server/src/main/java/uk/co/serin/thule/edge/ApplicationConfigurer.java
+++
@@ -1,9 +0,0 @@
-package uk.co.serin.thule.edge;
-
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@EnableZuulProxy
-public class ApplicationConfigurer {
-}
