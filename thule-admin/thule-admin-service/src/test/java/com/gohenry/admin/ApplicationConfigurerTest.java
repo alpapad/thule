07ed9d9141e5ae6a9ebf09ca810cf91506a07810
wayne <wayne@serin-consultancy.co.uk>
2018-09-25 14:26:19 +0100

    Thule synchronization

--- thule-admin/thule-admin-service/src/test/java/com/gohenry/admin/ApplicationConfigurerTest.java
+++ thule-admin/thule-admin-service/src/test/java/com/gohenry/admin/ApplicationConfigurerTest.java
@@ -2,24 +2,21 @@ package com.gohenry.admin;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
 import org.mockito.junit.MockitoJUnitRunner;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(MockitoJUnitRunner.class)
 public class ApplicationConfigurerTest {
-    @InjectMocks
-    private ApplicationConfigurer applicationConfigurer;
 
     @Test
     public void when_default_constructor_is_invoked_then_an_instance_is_instantiated() {
         // Given
 
         // When
-        ApplicationConfigurer applicationConfigurer = new ApplicationConfigurer();
+        ApplicationConfigurer sut = new ApplicationConfigurer();
 
         // Then
-        assertThat(applicationConfigurer).isNotNull();
+        assertThat(sut).isNotNull();
     }
 }
