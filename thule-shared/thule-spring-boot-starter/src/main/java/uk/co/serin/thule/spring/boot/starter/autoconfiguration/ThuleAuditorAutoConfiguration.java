bb438c5d33b47ec3cef9409d95cd4c4d0ea5ecc2
wayne <wayne@serin-consultancy.co.uk>
2019-01-27 18:01:24 +0000

    Resynced

--- thule-shared/thule-spring-boot-starter/src/main/java/uk/co/serin/thule/spring/boot/starter/autoconfiguration/ThuleAuditorAutoConfiguration.java
+++ thule-shared/thule-spring-boot-starter/src/main/java/uk/co/serin/thule/spring/boot/starter/autoconfiguration/ThuleAuditorAutoConfiguration.java
@@ -4,14 +4,12 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.data.domain.AuditorAware;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.security.core.Authentication;
 
 import uk.co.serin.thule.utils.service.data.auditor.AuditorConfigurer;
 
 @ConditionalOnClass({AuditorAware.class, Authentication.class})
 @Configuration
-@EnableJpaAuditing
 @Import(AuditorConfigurer.class)
 public class ThuleAuditorAutoConfiguration {
 }
