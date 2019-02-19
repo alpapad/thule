3df06368d3f744b9c1a758904a40ab85d8cf9ce2
wayne <wayne@serin-consultancy.co.uk>
2019-02-19 19:08:49 +0000

    Resync

--- thule-gateway/thule-gateway-service/src/main/java/uk/co/serin/thule/gateway/ApplicationProperties.java
+++ thule-gateway/thule-gateway-service/src/main/java/uk/co/serin/thule/gateway/ApplicationProperties.java
@@ -18,32 +18,11 @@ import lombok.ToString;
 public class ApplicationProperties {
     private HealthCheck healthCheck = new HealthCheck();
 
-    public HealthCheck getHealthCheck() {
-        return healthCheck;
-    }
-
-    public void setHealthCheck(HealthCheck healthCheck) {
-        this.healthCheck = healthCheck;
-    }
-
-    public class HealthCheck {
+    @Getter
+    @Setter
+    @ToString
+    public static class HealthCheck {
         private List<String> services = Collections.emptyList();
         private long timeout = 5000;
-
-        public List<String> getServices() {
-            return services;
-        }
-
-        public void setServices(List<String> services) {
-            this.services = services;
-        }
-
-        public long getTimeout() {
-            return timeout;
-        }
-
-        public void setTimeout(long timeout) {
-            this.timeout = timeout;
-        }
     }
 }
