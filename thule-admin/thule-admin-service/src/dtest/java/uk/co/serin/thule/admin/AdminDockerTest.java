b368524378bdd9286363eb83f8334ec885de98ec
wayne <wayne@serin-consultancy.co.uk>
2019-02-19 18:21:27 +0000

    Resync

--- thule-admin/thule-admin-service/src/dtest/java/uk/co/serin/thule/admin/AdminDockerTest.java
+++ thule-admin/thule-admin-service/src/dtest/java/uk/co/serin/thule/admin/AdminDockerTest.java
@@ -5,10 +5,9 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.actuate.health.Status;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.core.env.Environment;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import uk.co.serin.thule.test.assertj.ActuatorUri;
@@ -24,10 +23,11 @@ import static uk.co.serin.thule.test.assertj.ThuleAssertions.assertThat;
 @SpringBootTest
 public class AdminDockerTest {
     private static DockerCompose dockerCompose = new DockerCompose("src/dtest/docker/thule-admin-docker-tests/docker-compose.yml");
+    @Value("${thule.adminservice.api.host}")
+    private String adminServiceApiHost;
+    @Value("${thule.adminservice.api.port}")
+    private int adminServiceApiPort;
     private String adminServiceBaseUrl;
-
-    @Autowired
-    private Environment env;
 
     @BeforeClass
     public static void setUpClass() throws IOException {
@@ -41,16 +41,13 @@ public class AdminDockerTest {
 
     @Before
     public void setUp() {
-        // Create base url
-        var adminApiHost = env.getRequiredProperty("thule.adminservice.api.host");
-        int adminApiPort = env.getRequiredProperty("thule.adminservice.api.port", Integer.class);
-        adminServiceBaseUrl = String.format("http://%s:%s", adminApiHost, adminApiPort);
+        adminServiceBaseUrl = String.format("http://%s:%s", adminServiceApiPort, adminServiceApiPort);
     }
 
     @Test
     public void when_checking_health_then_status_is_up() {
         // Given
-        ActuatorUri actuatorUri = new ActuatorUri(URI.create(adminServiceBaseUrl + "/actuator/health"));
+        var actuatorUri = new ActuatorUri(URI.create(adminServiceBaseUrl + "/actuator/health"));
 
         // When/Then
         assertThat(actuatorUri).waitingForMaximum(Duration.ofMinutes(5)).hasHealthStatus(Status.UP);
