a20de7614a0606435e7476d4b3a475ac8441d2e0
wayne <wayne@serin-consultancy.co.uk>
2019-02-19 18:08:52 +0000

    Resync

--- thule-people/thule-people-service/src/dtest/java/uk/co/serin/thule/people/Application.java
+++ thule-people/thule-people-service/src/dtest/java/uk/co/serin/thule/people/Application.java
@@ -3,9 +3,6 @@ package uk.co.serin.thule.people;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import lombok.NoArgsConstructor;
-
-@NoArgsConstructor
 @SpringBootApplication
 public class Application {
     private static SpringApplication springApplication = new SpringApplication(Application.class);
