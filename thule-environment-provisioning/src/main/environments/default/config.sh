c72c53e8be4b047b24d58bdd53b78870bbdd1aac
wayne <wayne@serin-consultancy.co.uk>
2019-02-22 22:52:41 +0000

    Resync

--- thule-environment-provisioning/src/main/environments/default/config.sh
+++ thule-environment-provisioning/src/main/environments/default/config.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-OPTION_PROVISIONING_HOST=pooh
+PROVISIONING_HOST=pooh
 OPTION_STOP_CONTAINERS_DEFAULT=false
 OPTION_TARGET_DIRECTORY_DEFAULT=/home/wayne/.thule/thule-environment-provisioning
 OPTION_USERID_DEFAULT=wayne
