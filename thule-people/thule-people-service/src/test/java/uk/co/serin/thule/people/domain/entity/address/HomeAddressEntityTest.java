208bccdf8d582dd2e6cffe22ad12e5d9d3487d63
wayne <wayne@serin-consultancy.co.uk>
2019-02-27 19:51:11 +0000

    Resync

--- thule-people/thule-people-service/src/test/java/uk/co/serin/thule/people/domain/entity/address/HomeAddressEntityTest.java
+++ thule-people/thule-people-service/src/test/java/uk/co/serin/thule/people/domain/entity/address/HomeAddressEntityTest.java
@@ -2,22 +2,13 @@ package uk.co.serin.thule.people.domain.entity.address;
 
 import org.junit.Test;
 
-import uk.co.serin.thule.people.datafactory.MockReferenceDataFactory;
-import uk.co.serin.thule.people.datafactory.ReferenceDataFactory;
-import uk.co.serin.thule.people.domain.entity.country.CountryEntity;
-import uk.co.serin.thule.people.domain.model.state.StateCode;
-
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class HomeAddressEntityTest {
-    private ReferenceDataFactory referenceDataFactory = new MockReferenceDataFactory();
-
     @Test
     public void when_constructing_then_a_home_address_is_instantiated() {
         // When
-        var homeAddress = HomeAddressEntity.builder().addressLine1("Regent Street").addressLine2("Green")
-                                           .country(referenceDataFactory.getCountries().get(CountryEntity.GBR)).county("Greater London").postCode("EC4")
-                                           .state(referenceDataFactory.getStates().get(StateCode.ADDRESS_ENABLED)).town("London").build();
+        var homeAddress = HomeAddressEntity.builder().build();
 
         // Then
         assertThat(homeAddress).isNotNull();
--- thule-people/thule-people-service/src/test/java/uk/co/serin/thule/people/domain/entity/address/WorkAddressEntityTest.java
+++ thule-people/thule-people-service/src/test/java/uk/co/serin/thule/people/domain/entity/address/WorkAddressEntityTest.java
@@ -2,22 +2,13 @@ package uk.co.serin.thule.people.domain.entity.address;
 
 import org.junit.Test;
 
-import uk.co.serin.thule.people.datafactory.MockReferenceDataFactory;
-import uk.co.serin.thule.people.datafactory.ReferenceDataFactory;
-import uk.co.serin.thule.people.domain.entity.country.CountryEntity;
-import uk.co.serin.thule.people.domain.model.state.StateCode;
-
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class WorkAddressEntityTest {
-    private ReferenceDataFactory referenceDataFactory = new MockReferenceDataFactory();
-
     @Test
     public void when_constructing_then_a_work_address_is_instantiated() {
         // When
-        var workAddress = WorkAddressEntity.builder().addressLine1("Regent Street").addressLine2("Green")
-                                           .country(referenceDataFactory.getCountries().get(CountryEntity.GBR)).county("Greater London").postCode("EC4")
-                                           .state(referenceDataFactory.getStates().get(StateCode.ADDRESS_ENABLED)).town("London").build();
+        var workAddress = WorkAddressEntity.builder().build();
 
         // Then
         assertThat(workAddress).isNotNull();
