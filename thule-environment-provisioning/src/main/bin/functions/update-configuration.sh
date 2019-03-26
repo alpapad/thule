a27d9f1f9228f3a691be9bfc2c7e78939f15c2ee
wayne <wayne@serin-consultancy.co.uk>
2019-03-26 09:12:37 +0000

    Fixed provisioning bug when provisioning the configuration service

--- thule-environment-provisioning/src/main/bin/functions/update-configuration.sh
+++ thule-environment-provisioning/src/main/bin/functions/update-configuration.sh
@@ -12,26 +12,6 @@ function updateConfiguration {
     fi
 }
 
-function _updateConfigurationForSingleService {
-    # Input parameters
-    dockerComposeFile=$1
-    serviceName=$2
-
-    configDirectoryExpectedByDockerCompose=$(configDirectoryExpectedByDockerCompose ${dockerComposeFile})
-    tempConfigDirectory=${configDirectoryExpectedByDockerCompose}/temp/${serviceName}
-    configDirectory=${configDirectoryExpectedByDockerCompose}/${serviceName}
-
-    printf "Replacing old config ${configDirectory} with the new config ${tempConfigDirectory} ..."
-
-    rm -fr ${configDirectory}
-    mkdir -p ${configDirectory}
-
-    mv ${tempConfigDirectory}/* ${configDirectory}
-    rm -fr ${tempConfigDirectory}
-
-    printf "\033[32m done \033[0m \n"
-}
-
 function _updateConfigurationForAllServices {
     # Input parameters
     dockerComposeFile=$1
@@ -41,4 +21,29 @@ function _updateConfigurationForAllServices {
     do
         _updateConfigurationForSingleService ${dockerComposeFile} ${serviceName}
     done
+}
+
+function _updateConfigurationForSingleService {
+    # Input parameters
+    dockerComposeFile=$1
+    serviceName=$2
+
+    configDirectoryExpectedByDockerCompose=$(configDirectoryExpectedByDockerCompose ${dockerComposeFile})
+    tempConfigDirectory=${configDirectoryExpectedByDockerCompose}/temp/${serviceName}
+
+    if [[ "${serviceName}" == "thule-configuration-service" ]]; then
+        configDirectory=${configDirectoryExpectedByDockerCompose}
+        rm -f ${configDirectory}/*.yml
+    else
+        configDirectory=${configDirectoryExpectedByDockerCompose}/${serviceName}
+        rm -fr ${configDirectory}
+    fi
+
+    printf "Replacing old config ${configDirectory} with the new config ${tempConfigDirectory} ..."
+
+    mkdir -p ${configDirectory}
+    mv ${tempConfigDirectory}/* ${configDirectory}
+    rm -fr ${tempConfigDirectory}
+
+    printf "\033[32m done \033[0m \n"
 }
