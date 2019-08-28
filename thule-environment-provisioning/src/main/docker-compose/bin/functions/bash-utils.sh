61acb1cd59d45acff7131cdaf0e944f725f6d3a4
wayne <wayne@serin-consultancy.co.uk>
2019-08-28 08:21:01 +0100

    Polished provisioning scripts

--- thule-environment-provisioning/src/main/docker-compose/bin/functions/bash-utils.sh
+++ thule-environment-provisioning/src/main/docker-compose/bin/functions/bash-utils.sh
@@ -4,6 +4,6 @@ function toLowerCase {
     if [[ ((${BASH_VERSION%%.*} -ge 4)) ]]; then
         echo "${@,,}"   # Bash version >= 4.0
     else
-        echo "print '$@'.lower()" | python
+        echo "print '$@'.lower()" | python # To accomodate apple mac users who use bash 3.2 by default :-(
     fi
 }
