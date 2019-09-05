9d73bddd3d22aefdfeeaf070ac5e05da62cc9080
wayne <wayne@serin-consultancy.co.uk>
2019-09-05 12:12:24 +0100

    Polished provisioning scripts

--- thule-environment-provisioning/src/main/kubernetes/bin/functions/microk8s-utils.sh
+++ thule-environment-provisioning/src/main/kubernetes/bin/functions/microk8s-utils.sh
@@ -21,7 +21,7 @@ function deleteService() {
 }
 
 function installMicrok8s() {
-  if [[ "$(snap list microk8s)" != *"error"* ]]; then
+  if [[ $(snap info microk8s) ]]; then
     echo ""
     echo "================================================================================"
     echo "Skipping installation of microk8s because it is already installed"
