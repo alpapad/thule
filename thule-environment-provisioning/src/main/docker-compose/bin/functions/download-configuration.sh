da3914b1081fb4e3eea23159fec18aefc2888088
wayne <wayne@serin-consultancy.co.uk>
2019-10-22 11:33:57 +0100

    Removed config modules

--- thule-environment-provisioning/src/main/docker-compose/bin/functions/download-configuration.sh
+++ thule-environment-provisioning/src/main/docker-compose/bin/functions/download-configuration.sh
@@ -7,8 +7,7 @@ function downloadConfiguration() {
   echo ""
   echo "Downloading configuration..."
 
-  configName=${serviceName//service/config}
-  tempDirectory=$(mktemp -d --suffix "${configName}")
+  tempDirectory=$(mktemp -d --suffix "${serviceName}")
 
   # Locate the service version
   serviceVersion=$(grep "${NEXUS_HOST}:${NEXUS_PORT_DOCKER}/${serviceName}:" "${DOCKER_COMPOSE_FILE}" | sed "s/.*${NEXUS_HOST}:${NEXUS_PORT_DOCKER}\/${serviceName}:\(\S*\).*/\1/")
@@ -20,11 +19,11 @@ function downloadConfiguration() {
     exit 255
   fi
 
-  # Derive the config download url
+  # Derive the service download url
   if [[ ${serviceVersion} == *"SNAPSHOT" ]]; then
-    # Download the artifact metadata from nexus for the config using the Nexus REST api
-    metadataUrl="http://${NEXUS_HOST}:${NEXUS_PORT_MAVEN}/repository/maven-public/uk/co/serin/thule/${configName}/${serviceVersion}/maven-metadata.xml"
-    echo "Downloading ${configName} metadata using ${metadataUrl} from Nexus..."
+    # Download the artifact metadata from nexus for the service using the Nexus REST api
+    metadataUrl="http://${NEXUS_HOST}:${NEXUS_PORT_MAVEN}/repository/maven-public/uk/co/serin/thule/${serviceName}/${serviceVersion}/maven-metadata.xml"
+    echo "Downloading ${serviceName} metadata using ${metadataUrl} from Nexus..."
 
     mkdir -p "${tempDirectory}/metadata"
     pushd "${tempDirectory}/metadata"
@@ -39,36 +38,36 @@ function downloadConfiguration() {
 
     # Extract the latest version of the required snapshot
     timestampedSnapshotVersion=$(grep -m 1 \<value\> "${tempDirectory}/metadata/maven-metadata.xml" | sed "s/.*<value>\(.*\)<\/value>.*/\1/")
-    configDownloadUrl="http://${NEXUS_HOST}:${NEXUS_PORT_MAVEN}/repository/maven-public/uk/co/serin/thule/${configName}/${serviceVersion}/${configName}-${timestampedSnapshotVersion}.jar"
+    serviceDownloadUrl="http://${NEXUS_HOST}:${NEXUS_PORT_MAVEN}/repository/maven-public/uk/co/serin/thule/${serviceName}/${serviceVersion}/${serviceName}-${timestampedSnapshotVersion}.jar"
   else
-    configDownloadUrl="http://${NEXUS_HOST}:${NEXUS_PORT_MAVEN}/repository/maven-public/uk/co/serin/thule/${configName}/${serviceVersion}/${configName}-${serviceVersion}.jar"
+    serviceDownloadUrl="http://${NEXUS_HOST}:${NEXUS_PORT_MAVEN}/repository/maven-public/uk/co/serin/thule/${serviceName}/${serviceVersion}/${serviceName}-${serviceVersion}.jar"
   fi
 
-  # Download the config jar file using the Nexus REST api
+  # Download the service jar file using the Nexus REST api
   echo ""
-  echo "Downloading ${configDownloadUrl} from Nexus..."
+  echo "Downloading ${serviceDownloadUrl} from Nexus..."
 
   mkdir -p "${tempDirectory}/jar"
   pushd "${tempDirectory}/jar"
-  curlResponseCode=$(curl -L -O -w "%{http_code}" "${configDownloadUrl}")
+  curlResponseCode=$(curl -L -O -w "%{http_code}" "${serviceDownloadUrl}")
   popd
   if [[ ${curlResponseCode} -lt 200 || ${curlResponseCode} -gt 299 ]]; then
     echo ""
-    echo "ERROR: Received a HTTP status code of ${curlResponseCode} while trying to download ${configDownloadUrl}"
+    echo "ERROR: Received a HTTP status code of ${curlResponseCode} while trying to download ${serviceDownloadUrl}"
     echo "================================================================================"
     exit 255
   fi
 
-  configJarFile=$(echo "${configDownloadUrl}" | sed 's/.*\(${serviceName}-.*.jar\).*/\1/')
+  serviceJarFile=$(echo "${serviceDownloadUrl}" | sed 's/.*\(${serviceName}-.*.jar\).*/\1/')
 
   # Unzip the config files
   configDirectoryExpectedByConfigurationService=$(configDirectoryExpectedByConfigurationService)
   echo ""
-  echo "Unzipping ${configJarFile} to ${configDirectoryExpectedByConfigurationService}/temp/${serviceName}..."
+  echo "Unzipping ${serviceJarFile} to ${configDirectoryExpectedByConfigurationService}/temp/${serviceName}..."
 
   rm -fr "${configDirectoryExpectedByConfigurationService}/temp/${serviceName}"
   mkdir -p "${configDirectoryExpectedByConfigurationService}/temp/${serviceName}"
-  unzip -jq -d "${configDirectoryExpectedByConfigurationService}/temp/${serviceName}" "${tempDirectory}/jar/*.jar" config/application*.yml
+  unzip -jq -d "${configDirectoryExpectedByConfigurationService}/temp/${serviceName}" "${tempDirectory}/jar/*.jar" BOOT-INF/classes/config/application*.yml
 }
 
 function configDirectoryExpectedByConfigurationService() {
