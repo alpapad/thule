e44a172d719e5f146e34aba7e176fefa47186572
wayne <wayne@serin-consultancy.co.uk>
2019-10-30 19:47:07 +0000

    Added support for kubernetes service discovery

--- thule-admin-service/src/main/java/uk/co/serin/thule/admin/ApplicationConfigurer.java
+++ thule-admin-service/src/main/java/uk/co/serin/thule/admin/ApplicationConfigurer.java
@@ -2,12 +2,16 @@ package uk.co.serin.thule.admin;
 
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 import lombok.NoArgsConstructor;
 
 @Configuration
 @EnableAdminServer
+@EnableDiscoveryClient
+@EnableScheduling
 @NoArgsConstructor
 public class ApplicationConfigurer {
 }
