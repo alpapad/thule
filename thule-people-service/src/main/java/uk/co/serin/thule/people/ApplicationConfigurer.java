e8a2ad16d5fb0a2735051c93ffe2fec48fb6c41a
wayne <wayne@serin-consultancy.co.uk>
2019-10-31 10:22:08 +0000

    Added support for kubernetes service discovery

--- thule-people-service/src/main/java/uk/co/serin/thule/people/ApplicationConfigurer.java
+++ thule-people-service/src/main/java/uk/co/serin/thule/people/ApplicationConfigurer.java
@@ -1,5 +1,6 @@
 package uk.co.serin.thule.people;
 
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -11,6 +12,7 @@ import lombok.NoArgsConstructor;
 
 @Configuration
 @EnableAsync
+@EnableDiscoveryClient
 @EnableFeignClients
 @EnableJpaRepositories(repositoryBaseClass = ThuleJpaRepository.class)
 @NoArgsConstructor
