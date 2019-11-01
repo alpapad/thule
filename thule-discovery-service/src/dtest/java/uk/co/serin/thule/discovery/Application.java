f3abb634caee5a577318dab9587d4e4cfe5c0b60
wayne <wayne@serin-consultancy.co.uk>
2019-11-01 22:42:54 +0000

    Added support for kubernetes

--- thule-discovery-service/src/dtest/java/uk/co/serin/thule/discovery/Application.java
+++ thule-discovery-service/src/dtest/java/uk/co/serin/thule/discovery/Application.java
@@ -2,9 +2,7 @@ package uk.co.serin.thule.discovery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@EnableFeignClients
 @SpringBootApplication
 public class Application {
     private static SpringApplication springApplication = new SpringApplication(Application.class);
--- thule-discovery-service/src/dtest/java/uk/co/serin/thule/discovery/DiscoveryDockerTest.java
+++ thule-discovery-service/src/dtest/java/uk/co/serin/thule/discovery/DiscoveryDockerTest.java
@@ -1,48 +1,32 @@
 package uk.co.serin.thule.discovery;
 
 import org.junit.AfterClass;
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.actuate.health.Status;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import uk.co.serin.thule.test.assertj.ActuatorUri;
 import uk.co.serin.thule.utils.docker.DockerCompose;
 
 import java.io.IOException;
 import java.time.Duration;
 
-import static org.awaitility.Awaitility.given;
-import static org.awaitility.pollinterval.FixedPollInterval.fixed;
 import static uk.co.serin.thule.test.assertj.ThuleAssertions.assertThat;
 
-/**
- * Tests for the Discovery service.
- *
- * The objective of these tests is to prove that a service registered within the Discovery Service can be retrieved and executed successfully:
- *
- * <ul>
- * <li>Start the discovery service</li>
- * <li>Register a target service in the Discovery Service</li>
- * Rather than create another service to act as the target to test the Discovery Service, we will use the Discovery Service itself as the target to test the Discovery Service.
- * To that end the Discovery Service has been configured for these tests to register itself at startup.
- * <li>Retrieve a target service from the Discovery Service</li>
- * In other words, retrieve the Discovery Service to act as the target from the Discovery Service
- * <li>Invoke an endpoint on the target service</li>
- * Use the actuator on the target Discovery Service
- * </ul>
- */
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class DiscoveryDockerTest {
     private static final DockerCompose DOCKER_COMPOSE = new DockerCompose("src/dtest/docker/docker-compose.yml");
-    @Autowired
-    private ActuatorClient actuatorClient;
-    @Autowired
-    private DiscoveryClient discoveryClient;
+    @Value("${thule.discoveryservice.api.host}")
+    private String discoveryServiceApiHost;
+    @Value("${thule.discoveryservice.api.port}")
+    private int discoveryServiceApiPort;
+    private String discoveryServiceBaseUrl;
 
     @BeforeClass
     public static void setUpClass() throws IOException {
@@ -54,17 +38,17 @@ public class DiscoveryDockerTest {
         DOCKER_COMPOSE.down();
     }
 
+    @Before
+    public void setUp() {
+        discoveryServiceBaseUrl = String.format("http://%s:%s", discoveryServiceApiHost, discoveryServiceApiPort);
+    }
+
     @Test
-    public void when_checking_health_of_a_service_via_the_discovery_service_then_its_status_is_up() {
+    public void when_checking_health_then_status_is_up() {
         // Given
-        given().ignoreExceptions().pollInterval(fixed(Duration.ofSeconds(5))).
-                await().timeout(Duration.ofMinutes(5)).
-                       untilAsserted(() -> assertThat(discoveryClient.getServices()).contains("thule-discovery-service"));
+        var actuatorUri = ActuatorUri.of(discoveryServiceBaseUrl + "/actuator/health");
 
-        // When
-        var actualHealth = actuatorClient.health();
-
-        // Then
-        assertThat(actualHealth.get("status")).isEqualTo(Status.UP.getCode());
+        // When/Then
+        assertThat(actuatorUri).waitingForMaximum(Duration.ofMinutes(5)).hasHealthStatus(Status.UP);
     }
 }
