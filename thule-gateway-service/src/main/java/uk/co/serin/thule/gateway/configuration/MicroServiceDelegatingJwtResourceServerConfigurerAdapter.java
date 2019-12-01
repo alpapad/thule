804873b38f093e10e4f161d0e2bee60a4674a63d
wayne <wayne@serin-consultancy.co.uk>
2019-12-01 20:41:54 +0000

    Polish

--- thule-gateway-service/src/main/java/uk/co/serin/thule/gateway/configuration/MicroServiceDelegatingJwtResourceServerConfigurerAdapter.java
+++ thule-gateway-service/src/main/java/uk/co/serin/thule/gateway/configuration/MicroServiceDelegatingJwtResourceServerConfigurerAdapter.java
@@ -5,15 +5,14 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
-
-import uk.co.serin.thule.security.oauth2.resourceserver.JwtResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 
 import lombok.NoArgsConstructor;
 
 @Configuration
 @EnableResourceServer
 @NoArgsConstructor
-public class MicroServiceDelegatingJwtResourceServerConfigurerAdapter extends JwtResourceServerConfigurerAdapter {
+public class MicroServiceDelegatingJwtResourceServerConfigurerAdapter extends ResourceServerConfigurerAdapter {
     @Override
     public void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
