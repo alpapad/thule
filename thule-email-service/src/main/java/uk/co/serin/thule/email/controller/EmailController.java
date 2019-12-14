1ab055f114f32c050453ac4e4521d8db08fb9f2f
wayne <wayne@serin-consultancy.co.uk>
2019-12-14 20:29:20 +0000

    Changed k8s port for the configuration service to from 8080 to 80

--- thule-email-service/src/main/java/uk/co/serin/thule/email/controller/EmailController.java
+++ thule-email-service/src/main/java/uk/co/serin/thule/email/controller/EmailController.java
@@ -1,6 +1,5 @@
 package uk.co.serin.thule.email.controller;
 
-
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
