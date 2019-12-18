47dd17ba9e4c0f2283319aa71ff2f190d0ad8600
wayne <wayne@serin-consultancy.co.uk>
2019-12-18 18:01:29 +0000

    Synchronization

--- thule-admin-service/src/main/java/uk/co/serin/thule/admin/ApplicationConfigurer.java
+++ thule-admin-service/src/main/java/uk/co/serin/thule/admin/ApplicationConfigurer.java
@@ -3,10 +3,8 @@ package uk.co.serin.thule.admin;
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.web.server.adapter.ForwardedHeaderTransformer;
 
 import lombok.NoArgsConstructor;
 
@@ -16,8 +14,4 @@ import lombok.NoArgsConstructor;
 @EnableScheduling
 @NoArgsConstructor
 public class ApplicationConfigurer {
-    @Bean
-    public ForwardedHeaderTransformer forwardedHeaderTransformer() {
-        return new ForwardedHeaderTransformer();
-    }
 }
