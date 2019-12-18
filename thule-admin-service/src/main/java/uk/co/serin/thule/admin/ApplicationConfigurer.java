899f47914531563e6ed982c8bb1f4b084b9ac0b7
wayne <wayne@serin-consultancy.co.uk>
2019-12-18 14:01:55 +0000

    Added sba context-path

--- thule-admin-service/src/main/java/uk/co/serin/thule/admin/ApplicationConfigurer.java
+++ thule-admin-service/src/main/java/uk/co/serin/thule/admin/ApplicationConfigurer.java
@@ -3,8 +3,10 @@ package uk.co.serin.thule.admin;
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.server.adapter.ForwardedHeaderTransformer;
 
 import lombok.NoArgsConstructor;
 
@@ -14,4 +16,8 @@ import lombok.NoArgsConstructor;
 @EnableScheduling
 @NoArgsConstructor
 public class ApplicationConfigurer {
+    @Bean
+    public ForwardedHeaderTransformer forwardedHeaderTransformer() {
+        return new ForwardedHeaderTransformer();
+    }
 }
