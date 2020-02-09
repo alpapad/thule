53848e7b14646a4f6a6efe117542100b60d64c10
wayne <wayne@serin-consultancy.co.uk>
2020-02-09 16:28:35 +0000

    Migrated from TestRestTemplate to WebTestClient

--- thule-configuration-service/src/ctest/java/uk/co/serin/thule/configuration/HealthCheckContractTest.java
+++ thule-configuration-service/src/ctest/java/uk/co/serin/thule/configuration/HealthCheckContractTest.java
@@ -2,10 +2,9 @@ package uk.co.serin.thule.configuration;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.actuate.health.Status;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -19,13 +18,13 @@ import static uk.co.serin.thule.test.assertj.ThuleAssertions.assertThat;
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class HealthCheckContractTest {
-    @Autowired
-    private TestRestTemplate testRestTemplate;
+    @LocalServerPort
+    private int port;
 
     @Test
     public void when_checking_health_then_status_is_up() {
         // Given
-        var actuatorUri = ActuatorUri.using(testRestTemplate.getRootUri() + "/actuator/health");
+        var actuatorUri = ActuatorUri.using(String.format("http://localhost:%s/actuator/health", port));
 
         // When/Then
         assertThat(actuatorUri).waitingForMaximum(Duration.ofMinutes(5)).hasHealthStatus(Status.UP);
