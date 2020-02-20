fac054229cb809122956e5bb2c24c7755084f530
wayne <wayne@serin-consultancy.co.uk>
2020-02-20 13:49:51 +0000

    Migrate to shared 1.2.0

--- thule-gateway-service/src/main/java/uk/co/serin/thule/gateway/configuration/MicroServiceDelegatingResourceServerConfigurerAdapter.java
+++ thule-gateway-service/src/main/java/uk/co/serin/thule/gateway/configuration/MicroServiceDelegatingResourceServerConfigurerAdapter.java
@@ -2,13 +2,13 @@ package uk.co.serin.thule.gateway.configuration;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 import lombok.NoArgsConstructor;
 
 @Configuration
 @NoArgsConstructor
-public class MicroServiceDelegatingResourceServerConfigurerAdapter extends ResourceServerConfigurerAdapter {
+public class MicroServiceDelegatingResourceServerConfigurerAdapter extends WebSecurityConfigurerAdapter {
     @Override
     public void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests().antMatchers("/thule-*-service/**").permitAll(); // allow micro-service urls to be unauthenticated (authentication is done in the down-stream micro-service)
