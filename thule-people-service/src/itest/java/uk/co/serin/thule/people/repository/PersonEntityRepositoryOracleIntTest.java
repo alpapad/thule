27803020cc0b50652e698ece5a2453db29d096bf
wayne <wayne@serin-consultancy.co.uk>
2020-03-29 18:25:06 +0100

    Migrated to using testcontainers

--- thule-people-service/src/itest/java/uk/co/serin/thule/people/repository/PersonEntityRepositoryOracleIntTest.java
+++ thule-people-service/src/itest/java/uk/co/serin/thule/people/repository/PersonEntityRepositoryOracleIntTest.java
@@ -2,6 +2,11 @@ package uk.co.serin.thule.people.repository;
 
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.OracleContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 /**
  * MySql, Oracle, H2 and HSQL embedded database drivers are on the itest classpath. By default, the H2
@@ -12,5 +17,14 @@ import org.springframework.test.context.ActiveProfiles;
  */
 @ActiveProfiles({"itest", "itest-oracle"})
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Testcontainers
 public class PersonEntityRepositoryOracleIntTest extends PersonEntityRepositoryBaseIntTest {
+    @Container
+    private static OracleContainer oracle =
+            new OracleContainer("pooh:8082/oracle-database:18.4.0-xe-initialized").withEnv("ORACLE_PDB", "system").withEnv("ORACLE_PWD", "oracle");
+
+    @DynamicPropertySource
+    private static void mysqlProperties(DynamicPropertyRegistry registry) {
+        registry.add("thule.peopleservice.oracle.port", oracle::getFirstMappedPort);
+    }
 }
