c834e7e3c7e7ff1fe4ea7648a3f10d2b8ad267b8
wayne <wayne@serin-consultancy.co.uk>
2020-04-02 11:49:38 +0100

    Migrated docker tests to testcontainers

--- thule-people-service/src/dtest/java/uk/co/serin/thule/people/PeopleDockerTest.java
+++ thule-people-service/src/dtest/java/uk/co/serin/thule/people/PeopleDockerTest.java
@@ -1,62 +1,92 @@
 package uk.co.serin.thule.people;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.actuate.health.Status;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.http.HttpStatus;
 import org.springframework.test.web.reactive.server.WebTestClient;
-import org.testcontainers.containers.DockerComposeContainer;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.containers.Network;
+import org.testcontainers.containers.wait.strategy.Wait;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 import uk.co.serin.thule.test.assertj.ActuatorUri;
 
-import java.io.File;
 import java.time.Duration;
+import java.util.HashMap;
 
+import static java.lang.Boolean.FALSE;
 import static uk.co.serin.thule.test.assertj.ThuleAssertions.assertThat;
 
 @Testcontainers
 @SpringBootTest
 public class PeopleDockerTest {
-    private static String baseUrl;
-
+    private static final String MYSQL_ALIAS = "mysql";
     @Container
-    private static DockerComposeContainer<?> dockerCompose =
-            new DockerComposeContainer<>(new File("src/dtest/docker/docker-compose.yml"))
-                    .withExposedService("thule-people-service_1", 8080)
-                    .withLocalCompose(true);
+    private static MySQLContainer<?> mailhog = createMySqlContainer();
+    @Container
+    private static GenericContainer<?> springBootService = createSpringBootService();
+    private String baseUrl;
 
-    @DynamicPropertySource
-    private static void baseUrl(DynamicPropertyRegistry registry) {
-        baseUrl = String.format("http://localhost:%s", dockerCompose.getServicePort("thule-people-service_1", 8080));
+    private static MySQLContainer<?> createMySqlContainer() {
+        return new MySQLContainer<>("mysql").withNetwork(Network.SHARED).withNetworkAliases(MYSQL_ALIAS).withUsername("root").withPassword(null);
+    }
+
+    private static GenericContainer<?> createSpringBootService() {
+        var environmentVariables = new HashMap<>();
+        environmentVariables.put("JDK_JAVA_OPTIONS", "-XX:InitialHeapSize=256m -XX:MaxHeapSize=256m -XX:MaxMetaspaceSize=256m");
+        environmentVariables.put("SPRING_CLOUD_CONFIG_ENABLED", FALSE.toString());
+        environmentVariables.put("SPRING_CLOUD_KUBERNETES_ENABLED", FALSE.toString());
+        environmentVariables.put("SPRING_DATASOURCE_USERNAME", "root");
+        environmentVariables.put("SPRING_FLYWAY_ENABLED", FALSE.toString());
+        environmentVariables.put("SPRING_ZIPKIN_ENABLED", FALSE.toString());
+        environmentVariables.put("THULE_PEOPLESERVICE_MYSQL_HOST", MYSQL_ALIAS);
+        environmentVariables.put("THULE_PEOPLESERVICE_MYSQL_SCHEMA", "mysql");
+        environmentVariables.put("THULE_SHARED_LOGGING_LOGSTASH_ENABLED", FALSE.toString());
+        environmentVariables.put("THULE_SHARED_OAUTH2_RESOURCESERVER_JWS_ENABLED", FALSE.toString());
+        environmentVariables.put("TZ", "Europe/London");
+
+        return new GenericContainer("pooh:8084/thule-people-service")
+                .dependsOn(mailhog)
+                .waitingFor(Wait.forHttp("/actuator/health").forStatusCode(HttpStatus.OK.value()))
+                .withEnv(environmentVariables)
+                .withExposedPorts(8080)
+                .withNetwork(Network.SHARED);
+    }
+
+    @BeforeEach
+    public void beforeEach() {
+        baseUrl = String.format("http://%s:%s", springBootService.getContainerIpAddress(), springBootService.getFirstMappedPort());
     }
 
     @Test
     public void given_docker_container_has_been_started_when_checking_health_then_status_is_up() {
-        waitForTheApplicationToInitialize();
-    }
-
-    private void waitForTheApplicationToInitialize() {
+        // Given
         var actuatorUri = ActuatorUri.using(baseUrl + "/actuator/health");
-        assertThat(actuatorUri).waitingForMaximum(Duration.ofMinutes(5)).hasHealthStatus(Status.UP);
+
+        // When
+        assertThat(actuatorUri)
+                .waitingForMaximum(Duration.ofMinutes(5))
+
+                //Then
+                .hasHealthStatus(Status.UP);
     }
 
     @Test
     public void given_docker_container_has_been_started_when_retrieving_actuator_info_then_it_shows_correct_microservice_is_in_docker_container() {
-        // Given
-        waitForTheApplicationToInitialize();
-
         // When
-        WebTestClient.bindToServer().baseUrl(baseUrl).build()
-                     .get().uri("/actuator/info")
-                     .exchange()
-                     .expectStatus().isOk()
-                     .expectBody()
+        WebTestClient
+                .bindToServer().baseUrl(baseUrl).build()
+                .get().uri("/actuator/info")
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody()
 
-                     // Then
-                     .jsonPath("$.name").isNotEmpty()
-                     .jsonPath("$.name").isEqualTo("thule-people-service");
+                // Then
+                .jsonPath("$.name").isNotEmpty()
+                .jsonPath("$.name").isEqualTo("thule-people-service");
     }
 }
