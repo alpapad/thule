749a932d1202248e28bbb3855042f61e5b7c3196
wayne <wayne@serin-consultancy.co.uk>
2020-06-27 19:53:28 +0100

    Changed default path of rest docs

--- thule-email-service/src/main/java/uk/co/serin/thule/email/SwaggerConfiguration.java
+++ thule-email-service/src/main/java/uk/co/serin/thule/email/SwaggerConfiguration.java
@@ -7,14 +7,11 @@ import org.springframework.context.annotation.Configuration;
 
 import uk.co.serin.thule.email.controller.EmailController;
 
-import javax.servlet.ServletContext;
-
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.paths.RelativePathProvider;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
@@ -25,22 +22,11 @@ public class SwaggerConfiguration {
     private BuildProperties buildProperties;
 
     @Bean
-    public Docket api(ServletContext servletContext) {
-        return new Docket(DocumentationType.SWAGGER_2).pathProvider(new RelativePathProvider(servletContext) {
-            @Override
-            protected String getDocumentationPath() {
-                return super.getDocumentationPath() + "/swagger";
-            }
-
-            @Override
-            public String getApplicationBasePath() {
-                return super.getApplicationBasePath() + "/swagger";
-            }
-        })
-                                                      .apiInfo(apiInfo()).host("piglet")
-                                                      .pathMapping("thule-email-service")
-                                                      .select().apis(RequestHandlerSelectors.basePackage(EmailController.class.getPackageName()))
-                                                      .paths(PathSelectors.any()).build();
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo()).host("piglet").pathMapping("thule-email-service")
+                .select().apis(RequestHandlerSelectors.basePackage(EmailController.class.getPackageName()))
+                .paths(PathSelectors.any()).build();
     }
 
     private ApiInfo apiInfo() {
