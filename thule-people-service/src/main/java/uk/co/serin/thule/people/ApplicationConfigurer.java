6a6213d09e7f4426a55e9db4e42c079f1a2e2451
wayne <wayne@serin-consultancy.co.uk>
2020-07-07 14:20:56 +0100

    Temporarily exclude security

--- thule-people-service/src/main/java/uk/co/serin/thule/people/ApplicationConfigurer.java
+++ thule-people-service/src/main/java/uk/co/serin/thule/people/ApplicationConfigurer.java
@@ -1,11 +1,14 @@
 package uk.co.serin.thule.people;
 
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import uk.co.serin.thule.feign.EnableFeignJwtClients;
 import uk.co.serin.thule.people.repository.support.ThuleJpaRepository;
@@ -23,5 +26,15 @@ import lombok.NoArgsConstructor;
 public class ApplicationConfigurer {
     static {
         SecurityContextHolder.setStrategyName(SecurityContextHolder.MODE_INHERITABLETHREADLOCAL);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedMethods("*").allowedOrigins("*");
+            }
+        };
     }
 }
